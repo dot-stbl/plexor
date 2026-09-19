bc5148ee46febfd9cef6f9102f7a9e313f0d1014
bradw <bradw@plexor.dev>
2026-09-20 01:16:03 +0500

    [.stbl](feat/fe): fix storybook merge fallout (dup dep key, dup ctx hoist)

--- web/apps/console/src/shared/ui/primitives/tooltip.tsx
+++ web/apps/console/src/shared/ui/primitives/tooltip.tsx
@@ -56,7 +56,6 @@ function TooltipRoot({ children, open, ...props }: TooltipRootProps) {
   // Pattern A: children are [<TooltipTrigger>, <TooltipContent>] siblings.
   // Pass them through to RAC's <TooltipTrigger>.
   const arr = React.Children.toArray(children)
-  const ctx = React.useContext(TooltipContext)
   if (arr.length === 2) {
     const trigger = arr[0]!
     const content = arr[1]!
